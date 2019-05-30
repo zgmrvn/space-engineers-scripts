768a7cbce677b5e23baf0a6c9cfddc2a6f4c8a73
zgmrvn <zgmrvn@gmail.com>
2019-05-30 09:28:21 +0200

    fixed script breaking when no item in container

--- ingot.cs
+++ ingot.cs
@@ -28,6 +28,11 @@ public void Main(string argument, UpdateType updateSource)
         var items = new List<MyInventoryItem>();
         inventory.GetItems(items);
 
+        if (items.Count == 0)
+        {
+            continue;
+        }
+
         // Prepare strings.
         string type = items[0].Type.ToString().Split('/')[1];
         string tons = ((float)inventory.CurrentMass / 1000).ToString("F1", System.Globalization.CultureInfo.InvariantCulture) + " t";
