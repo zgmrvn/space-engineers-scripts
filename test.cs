a6db2be42ec886e1e40a922cb79837f183842cb8
zgmrvn <zgmrvn@gmail.com>
2019-06-09 10:18:07 +0200

    basic move program

--- test.cs
+++ test.cs
@@ -1,14 +1,78 @@
-private IMyShipConnector connector;
+private List<IMyTerminalBlock> wheels;
+private IMyShipController remoteControl;
+private double velocity = 0;
+private double targetVelocity = 2;
 
 public Program()
 {
     Runtime.UpdateFrequency = UpdateFrequency.Update10;
 
-    connector = GridTerminalSystem.GetBlockWithName("Connector Front") as IMyShipConnector;
-    connector.Enabled = false;
+    // Wheels.
+    wheels = new List<IMyTerminalBlock>();
+    (GridTerminalSystem.GetBlockGroupWithName("Wheels") as IMyBlockGroup).GetBlocks(wheels);
+
+    // Remote control.
+    remoteControl = GridTerminalSystem.GetBlockWithName("Remote Control") as IMyShipController;
+
+    // GO !
+    InvertPropulsion();
+    ToggleWheels(true);
 }
 
 public void Main(string argument, UpdateType updateSource)
 {
-    Echo(connector.Enabled.ToString());
+    velocity = remoteControl.GetShipVelocities().LinearVelocity.Length() * 3600 / 1000;
+
+
+    if (velocity < targetVelocity - 1)
+    {
+        ToggleWheels(true);
+    }
+
+    else if (velocity > targetVelocity + 1)
+    {
+        ToggleBrakes(true);
+    }
+
+    else if (velocity > targetVelocity)
+    {
+        ToggleBrakes(false);
+        ToggleWheels(false);
+    }
+}
+
+private void ToggleWheels(bool state)
+{
+    if (state)
+    {
+        ToggleBrakes(false);
+    }
+
+    string action = "OnOff_" + (state ? "On" : "Off");
+    
+    foreach (IMyMotorSuspension wheel in wheels)
+    {
+        wheel.ApplyAction(action);
+    }
+}
+
+private void ToggleBrakes(bool state)
+{
+    if (state)
+    {
+        ToggleWheels(false);
+    }
+
+    foreach (IMyMotorSuspension wheel in wheels)
+    {
+        wheel.Brake = state;
+    } 
+}
+
+private void InvertPropulsion()
+{
+    foreach (IMyMotorSuspension wheel in wheels)
+    {
+        wheel.InvertPropulsion = !wheel.InvertPropulsion;
+    } 
 }
